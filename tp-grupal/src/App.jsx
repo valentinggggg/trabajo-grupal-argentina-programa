d5df2c5d86a7b4c2bc9fb426c82006b3bc952a73
valentinggggg <garellovalentin@gmail.com>
2023-11-26 18:57:20 -0300

    trabajo terminado
    trabajo terminado

--- tp-grupal/src/App.jsx
+++ tp-grupal/src/App.jsx
@@ -4,6 +4,7 @@ import Inicio from './components/Inicio'
 import SobreNos from './components/SobreNos'
 import Contact from './components/Contact'
 import Combis from './components/Combis'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <SobreNos />
       <Combis />
       <Contact />
+      <Footer />
     </>
   )
 }
